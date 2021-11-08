a242d6bd49556b8f14e02765668859b50d701494
olusolaanikaneye <olusolaanikaneye@gmail.com>
2021-11-08 17:39:04 +0100

    DataTable Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
-import { DatePicker, message } from 'antd';
+import { DatePicker, message, Table } from 'antd';
 import 'antd/dist/antd.css';
 import './style.css';
+
+const dataSource = [
+  {
+    key: '1',
+    name: 'Sola',
+    age: 30,
+    address: '10 Downing Street',
+  },
+
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '12 Downing street',
+  },
+];
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+];
 
 export default function App() {
   const [date, setDate] = useState(null);
@@ -21,6 +50,7 @@ export default function App() {
       />
       <div style={{ marginTop: 16 }}>
         selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+        <Table dataSource={dataSource} columns={columns} />
       </div>
     </div>
   );
