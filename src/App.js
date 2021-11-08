a9a3f446ef8b0edfb791472944fbcef6d49a8ea3
olusolaanikaneye <olusolaanikaneye@gmail.com>
2021-11-08 12:40:48 +0100

    Basic setup

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import 'antd/dist/antd.css';
 import './style.css';
 
 export default function App() {
+  const [date, setDate] = useState(null);
+  const handleChange = (value) => {
+    message.info(
+      `selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`
+    );
+    setDate(value);
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={{ width: 400, margin: '100px auto', color: 'blue' }}>
+      Hello from Luca Pacioli, pick date
+      <DatePicker
+        onChange={handleChange}
+        style={{ Backgroundcolor: 'green' }}
+      />
+      <div style={{ marginTop: 16 }}>
+        selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+      </div>
     </div>
   );
 }
