4a0cabab49e57cd93ff6c155fb51d18da3eaffe6
olusolaanikaneye <olusolaanikaneye@gmail.com>
2021-11-08 18:12:47 +0100

    Daybook data Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
-import { DatePicker, message, Table } from 'antd';
+import { DatePicker, message, Table, Button } from 'antd';
 import 'antd/dist/antd.css';
 import './style.css';
+
+const daybookData = [
+  {
+    key: '1',
+    itemName: 'Gold Pendant',
+    price: 50,
+    discount: 'none',
+    date: '31 st july',
+  },
+
+  {
+    key: '2',
+    itemName: 'Diamond Pendant',
+    price: 90,
+    discount: 'none',
+    date: '21st july',
+  },
+
+  {
+    key: '3',
+    itemName: 'Gold Necklace',
+    price: 80,
+    discount: 'none',
+    date: '11th july',
+  },
+];
 
 const dataSource = [
   {
@@ -30,6 +56,36 @@ const columns = [
     dataIndex: 'age',
     key: 'age',
   },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+const dataColumns = [
+  {
+    title: 'ItemName',
+    dataIndex: 'itemName',
+    key: 'itemName',
+  },
+
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price',
+  },
+
+  {
+    title: 'Discount',
+    dataIndex: 'discount',
+    key: 'discount',
+  },
+
+  {
+    title: 'Date',
+    dataIndex: 'date',
+    key: 'date',
+  },
 ];
 
 export default function App() {
@@ -50,6 +106,8 @@ export default function App() {
       />
       <div style={{ marginTop: 16 }}>
         selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+        <Table dataSource={daybookData} columns={dataColumns} />
+        <Button />
         <Table dataSource={dataSource} columns={columns} />
       </div>
     </div>
