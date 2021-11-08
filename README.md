896bfe154a2c9e9def8a0e4e03f8c3acd8936a3d
olusolaanikaneye <olusolaanikaneye@gmail.com>
2021-11-08 12:25:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-cyrdvc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-cyrdvc)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { DatePicker, message } from 'antd';
+import 'antd/dist/antd.css';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
